30384ae872b966924deba9ed4a0ce21adab10f3c
Yacqub Mohamed <yam22@cornell.edu>
2024-03-10 22:42:53 -0400

    Extended Grass Memory by 4KB for Reg File

--- grass/process.h
+++ grass/process.h
@@ -3,7 +3,10 @@
 #include "elf.h"
 #include "disk.h"
 
-enum {
+#define NUM_REGS 32
+
+enum
+{
     PROC_UNUSED,
     PROC_LOADING, /* allocated and wait for loading elf binary */
     PROC_READY,   /* finished loading elf and wait for first running */
@@ -13,29 +16,31 @@ enum {
     PROC_WAIT_TO_RECV
 };
 
-struct process{
+struct process
+{
     int pid;
     int status;
     int receiver_pid; /* used when waiting to send a message */
     void *sp, *mepc;  /* process context = stack pointer (sp)
                        * + machine exception program counter (mepc) */
+    int reg_file[NUM_REGS];
 };
 
-#define MAX_NPROCESS  16
+#define MAX_NPROCESS 16
 extern int proc_curr_idx;
 extern struct process proc_set[MAX_NPROCESS];
-#define curr_pid      proc_set[proc_curr_idx].pid
-#define curr_status   proc_set[proc_curr_idx].status
+#define curr_pid proc_set[proc_curr_idx].pid
+#define curr_status proc_set[proc_curr_idx].status
 
 void intr_entry(int);
 void excp_entry(int);
 
-int  proc_alloc();
+int proc_alloc();
 void proc_free(int);
-void proc_set_ready (int);
-void proc_set_running (int);
-void proc_set_runnable (int);
+void proc_set_ready(int);
+void proc_set_running(int);
+void proc_set_runnable(int);
 
 void ctx_entry(void);
-void ctx_start(void** old_sp, void* new_sp);
-void ctx_switch(void** old_sp, void* new_sp);
+void ctx_start(void **old_sp, void *new_sp);
+void ctx_switch(void **old_sp, void *new_sp);
--- library/egos.h
+++ library/egos.h
@@ -1,24 +1,25 @@
 #pragma once
 
-struct earth {
+struct earth
+{
     /* CPU interface */
     int (*timer_reset)();
 
     int (*intr_register)(void (*handler)(int));
     int (*excp_register)(void (*handler)(int));
 
-    int (*mmu_alloc)(int* frame_no, void** cached_addr);
+    int (*mmu_alloc)(int *frame_no, void **cached_addr);
     int (*mmu_free)(int pid);
     int (*mmu_map)(int pid, int page_no, int frame_no);
     int (*mmu_switch)(int pid);
 
     /* Devices interface */
-    int (*disk_read)(int block_no, int nblocks, char* dst);
-    int (*disk_write)(int block_no, int nblocks, char* src);
+    int (*disk_read)(int block_no, int nblocks, char *dst);
+    int (*disk_write)(int block_no, int nblocks, char *src);
 
     int (*tty_recv_intr)();
-    int (*tty_read)(char* buf, int len);
-    int (*tty_write)(char* buf, int len);
+    int (*tty_read)(char *buf, int len);
+    int (*tty_write)(char *buf, int len);
 
     int (*tty_printf)(const char *format, ...);
     int (*tty_info)(const char *format, ...);
@@ -27,57 +28,65 @@ struct earth {
     int (*tty_critical)(const char *format, ...);
 
     /* Some information about earth layer configuration */
-    enum { QEMU, ARTY } platform;
-    enum { PAGE_TABLE, SOFT_TLB } translation;
+    enum
+    {
+        QEMU,
+        ARTY
+    } platform;
+    enum
+    {
+        PAGE_TABLE,
+        SOFT_TLB
+    } translation;
 };
 
-struct grass {
+struct grass
+{
     /* Shell environment variables */
     int workdir_ino;
     char workdir[128];
 
     /* Process control interface */
-    int  (*proc_alloc)();
+    int (*proc_alloc)();
     void (*proc_free)(int pid);
     void (*proc_set_ready)(int pid);
 
     /* System call interface */
     void (*sys_exit)(int status);
-    int  (*sys_send)(int pid, char* msg, int size);
-    int  (*sys_recv)(int* pid, char* buf, int size);
+    int (*sys_send)(int pid, char *msg, int size);
+    int (*sys_recv)(int *pid, char *buf, int size);
 };
 
 extern struct earth *earth;
 extern struct grass *grass;
 
 /* Memory layout */
-#define PAGE_SIZE          4096
-#define FRAME_CACHE_END    0x80020000
-#define FRAME_CACHE_START  0x80004000  /* 112KB  frame cache           */
-                                       /*        earth interface       */
-#define GRASS_STACK_TOP    0x80003f80  /* 8KB    earth/grass stack     */
-                                       /*        grass interface       */
-#define APPS_STACK_TOP     0x80002000  /* 6KB    app stack             */
-#define SYSCALL_ARG        0x80000400  /* 1KB    system call args      */
-#define APPS_ARG           0x80000000  /* 1KB    app main() argc, argv */
-#define APPS_SIZE          0x00003000  
-#define APPS_ENTRY         0x08005000  /* 12KB   app code+data         */
-#define GRASS_SIZE         0x00002800
-#define GRASS_ENTRY        0x08002800  /* 8KB    grass code+data       */
-                                       /* 12KB   earth data            */
-                                       /* earth code is in QSPI flash  */
-
+#define PAGE_SIZE 4096
+#define FRAME_CACHE_END 0x80020000
+#define FRAME_CACHE_START 0x80004000 /* 112KB  frame cache           */
+                                     /*        earth interface       */
+#define GRASS_STACK_TOP 0x80003f80   /* 8KB    earth/grass stack     */
+                                     /*        grass interface       */
+#define APPS_STACK_TOP 0x80002000    /* 6KB    app stack             */
+#define SYSCALL_ARG 0x80000400       /* 1KB    system call args      */
+#define APPS_ARG 0x80000000          /* 1KB    app main() argc, argv */
+#define APPS_SIZE 0x00003000
+#define APPS_ENTRY 0x08006000 /* 12KB   app code+data         */
+#define GRASS_SIZE 0x00003800
+#define GRASS_ENTRY 0x08002800 /* 8KB    grass code+data       */
+                               /* 12KB   earth data            */
+                               /* earth code is in QSPI flash  */
 
 #ifndef LIBC_STDIO
 /* Only earth/dev_tty.c uses LIBC_STDIO and does not need these macros */
-#define printf             earth->tty_printf
-#define INFO               earth->tty_info
-#define FATAL              earth->tty_fatal
-#define SUCCESS            earth->tty_success
-#define CRITICAL           earth->tty_critical
+#define printf earth->tty_printf
+#define INFO earth->tty_info
+#define FATAL earth->tty_fatal
+#define SUCCESS earth->tty_success
+#define CRITICAL earth->tty_critical
 #endif
 
 /* Memory-mapped I/O register access macros */
 #define ACCESS(x) (*(__typeof__(*x) volatile *)(x))
-#define REGW(base, offset) (ACCESS((unsigned int*)(base + offset)))
-#define REGB(base, offset) (ACCESS((unsigned char*)(base + offset)))
+#define REGW(base, offset) (ACCESS((unsigned int *)(base + offset)))
+#define REGB(base, offset) (ACCESS((unsigned char *)(base + offset)))
