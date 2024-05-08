828eb53b6b10da4bb90abd0b33f96f658ec98ddb
Yacqub Mohamed <yam22@cornell.edu>
2024-05-07 22:23:41 -0400

    cleanup comments

--- apps/system/sys_proc.c
+++ apps/system/sys_proc.c
@@ -45,7 +45,6 @@ int main()
             reply->type = app_spawn(req) < 0 ? CMD_ERROR : CMD_OK;
 
             /* Handling background processes */
-            // Sending kill message before we send an ok message to shell hangs
             shell_waiting = (req->argv[req->argc - 1][0] != '&');
             if (!shell_waiting && app_pid > 0)
                 INFO("process %d running in the background", app_pid);
